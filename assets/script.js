316a866f63fd7b291075d524da2bfe4890059a98
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-28 19:48:04 -0700

    added vars

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var text9 = $("#9hr");
+var text10 = $("#10hr");
+var text11 = $("#11hr");
+var text12 = $("#12hr");
+var text1 = $("#1hr");
+var text2 = $("#2hr");
+var text3 = $("#3hr");
+var text4 = $("#4hr");
+var text5 = $("#5hr");
+
+var textHr9 = localStorage.getItem("hr-9");
+var textHr10 = localStorage.getItem("hr-10");
+var textHr11 = localStorage.getItem("hr-11");
+var textaHrl12 = localStorage.getItem("hr-12");
+var textHr1 = localStorage.getItem("hr-1");
+var textHr2 = localStorage.getItem("hr-2");
+var textHr3 = localStorage.getItem("hr-3");
+var textHr4 = localStorage.getItem("hr-4");
+var textHr5 = localStorage.getItem("hr-5");
+
