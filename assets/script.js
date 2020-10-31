6e6c19982edb4398347000c2934cf4ed84fb9fee
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-31 00:55:49 -0700

    save text need more dynamic

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,46 @@
 $(document).ready(function () {
-    const currentTime = moment().format('dddd') + " , " + moment().format('LL');
+    const currentTime = moment().format('dddd') + ", " + moment().format('LL');
     $("#currentDay").text(currentTime)
     
-    const times = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+    const times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
     
-//     times.forEach(time => {
-//         const timeCheck = window.localStorage.getItem(time)
-//         const currentHour = moment().hour()
-//         const timeId = "#" + time
+    times.forEach(time => {
+        const timeCheck = window.localStorage.getItem(time)
+        const currentHour = moment().hour()
+        const timeId = "#" + time
     
-//     if (currentHour > time) {
-//         $("#input").addClass("bg-danger text-light")
-//         $(timeId).attr("disabled", true)
-//     } else if (currentHour === time) {
-//         $(timeId).addClass("bg-secondary text-light")
-//     } else {
-//         $(timeId).addClass("bg-success text-light")
-//     }
+    if (currentHour > time) {
+        $("#input").addClass("bg-danger text-light")
+        $(timeId).attr("disabled", true)
+    } else if (currentHour === time) {
+        $(timeId).addClass("bg-secondary text-light")
+    } else {
+        $(timeId).addClass("bg-success text-light")
+    }
     
-//         if (timeCheck === null) window.localStorage.setItem(time, "")
-//         if (timeCheck.length > 0) $(timeId).attr("value", window.localStorage.getItem(time))
-//     })
-// })
+        if (timeCheck === null) window.localStorage.setItem(time, "")
+        if (timeCheck.length > 0) $(timeId).attr("value", window.localStorage.getItem(time))
+    })
+// $(".saveBtn").on("click", function (e) {
+//     e.preventDefault()
+//     const time = e.target.querySelector("input").getAttribute("id")
+//     const text = e.target.querySelector("input").value
 
-$(".saveBtn").on("click", function (e) {
+//     window.localStorage.setItem(time, text)
+// })
+$("form").on("submit", function (e) {
     e.preventDefault()
+
     const time = e.target.querySelector("input").getAttribute("id")
     const text = e.target.querySelector("input").value
 
     window.localStorage.setItem(time, text)
+    })
+
 })
-})
+
+
+
 
 // $(".saveBtn").on("click", function(){
 //     userInput = $(this).siblings("textArea").val().trim();
