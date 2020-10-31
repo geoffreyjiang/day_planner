f29cab65418fcdd69bffa5e5e7b5e4edd68e522e
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-31 09:56:02 -0700

    button refreshes color works

--- assets/script.js
+++ assets/script.js
@@ -21,19 +21,13 @@ $(document).ready(function () {
         if (timeCheck === null) window.localStorage.setItem(time, "")
         if (timeCheck.length > 0) $(timeId).attr("value", window.localStorage.getItem(time))
     })
-// $(".saveBtn").on("click", function (e) {
-//     e.preventDefault()
-//     const time = e.target.querySelector("input").getAttribute("id")
-//     const text = e.target.querySelector("input").value
 
-//     window.localStorage.setItem(time, text)
-// })
 $("form").on("submit", function (e) {
     e.preventDefault()
 
     const time = e.target.querySelector("input").getAttribute("id")
     const text = e.target.querySelector("input").value
-
+    location.reload();
     window.localStorage.setItem(time, text)
     })
 
